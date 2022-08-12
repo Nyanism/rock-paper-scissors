b070bd508aa226d987158a19cf6c4fe85a202cd6
Nyanism <ojjstuff@gmail.com>
2022-08-12 16:33:51 +0800

    Implement playRound function
    The playRound function plays a round between the player and the computer
    when given both inputs.

--- javascript.js
+++ javascript.js
@@ -3,3 +3,36 @@ function getComputerChoice(){
 
   return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
+
+function playRound(playerSelection, computerSelection){
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  
+  if(playerSelection === computerSelection) {
+    return "It's a draw!";
+  }
+
+  if(playerSelection === "rock") {
+    if(computerSelection === "scissors"){
+      return "You Win! Rock beats Scissors";
+    } else {
+      return "You Lose! Paper beats Rock";
+    }
+  } 
+
+  if(playerSelection === "scissors") {
+    if(computerSelection === "paper"){
+      return "You Win! Scissors beats Paper";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  } 
+
+  if(playerSelection === "paper") {
+    if(computerSelection === "rock"){
+      return "You Win! Paper beats Rock";
+    } else {
+      return "You Lose! Scissors beats Paper";
+    }
+  } 
+}
